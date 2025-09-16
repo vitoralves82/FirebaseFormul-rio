db9b4c7d837d96080a4d07be06ba95770fec1421
vitoralves82 <vitoralves82@yahoo.com.br>
2025-09-16 00:15:14 -0300

    Update client.ts

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,4 +1,14 @@
 import { createClient, type SupabaseClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const createClient = () =>
+  createBrowserClient(
+    supabaseUrl!,
+    supabaseKey!,
+  );
 
 let browserClient: SupabaseClient | null = null;
 
