0dcb3697412cfeb7852824a744b6374037266d8c
vitoralves82 <vitoralves82@yahoo.com.br>
2025-09-16 22:07:59 -0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,12 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-if (!url || !key) {
-  // Ajuda durante build/local
-  // eslint-disable-next-line no-console
-  console.warn('⚠️ Defina NEXT_PUBLIC_SUPABASE_URL e NEXT_PUBLIC_SUPABASE_ANON_KEY no .env');
-}
-
-export const supabase = createClient(url, key);
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
